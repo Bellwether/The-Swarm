bd2b60995d7072b0d6472f17eba85f3ab7633a4a
Travis Dunn <travisdunn@Durandal.local>
2012-01-01 17:51:07 +0100

    deployment config

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,19 +3,23 @@ require 'bundler/capistrano'
 set :application, "the swarm"
 set :domain, "ec2-107-21-237-219.compute-1.amazonaws.com"
 
-set :repository,  "/home/ubuntu/deploy.git"
-set :local_repository,  "ssh://git@github.com:Bellwether/The-Swarm.git"
+set :repository,  "git@github.com:Bellwether/The-Swarm.git"
 
 set :scm, :git
 set :branch, "master"
 
+default_run_options[:pty] = true
 ssh_options[:keys] = [File.join(ENV["HOME"], ".ec2", "keypairs", "theswarm.pem")] # local workstation ec2 keypair pem
 ssh_options[:forward_agent] = true
-set :deploy_via, :remote_cache
+#set :deploy_via, :remote_cache
+
+default_run_options[:pty] = true
 set :deploy_to, "/home/ubuntu/www"
 set :user, "ubuntu"
 set :use_sudo, false
 set :scm_verbose, true
+set :git_enable_submodules, 1
+set :keep_releases, 2
 
 role :web, domain
 role :app, domain
