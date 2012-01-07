2f5e4daa537b051d6a649637fc75f664143fa482
Travis Dunn <travisdunn@Durandal.local>
2012-01-07 15:34:35 +0100

    validation error display

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,6 @@ class Contact < ActiveRecord::Base
   has_many :subscriptions, :dependent => :destroy
   
   validates_presence_of :name
+  validates_format_of :phone, :with => /^(1?(-?\d{3})-?)?(\d{3})(-?\d{4})$/, :message => 'must 7, 10, or 11 digit US phone number',
+                      :unless => Proc.new { |m| m.phone.blank? }  
 end
--- test/unit/contact_test.rb
+++ test/unit/contact_test.rb
@@ -10,4 +10,19 @@ class ContactTest < ActiveModel::TestCase
     assert_equal false, @contact.valid?
     assert_equal ["can't be blank"], @contact.errors[:name]
   end
+  
+  
+  def test_should_validate_phone
+    ['15615552323','1-561-555-1212','5613333'].each do |valid_number|
+      @contact.phone = valid_number
+      @contact.valid?
+      assert_equal [], @contact.errors[:phone]
+    end
+    
+    ['1-555-5555','15553333','0-561-555-1212'].each do |invalid_number|
+      @contact.phone = invalid_number
+      @contact.valid?
+      assert_equal ['must 7, 10, or 11 digit US phone number'], @contact.errors[:phone]
+    end    
+  end  
 end
